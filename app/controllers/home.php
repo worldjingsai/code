19ea7d1653149a0f7c7de4a4a87ea327e60702d2
2822676663@qq.com <2822676663@qq.com>
2014-01-04 00:31:17 +0800

    bbb

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -13,7 +13,16 @@ class Home extends SB_Controller{
     public function index (){
         $data['date'] = date("m月d日");
         $this->load->model('index_m');
-        $data['provices'] = $this->index_m->get_all_province();
+        $provinces = $this->index_m->get_all_province();
+        $universities = $this->index_m->get_all_university();
+        if(is_array($universities) && !empty($universities)){
+            foreach($universities as $university){
+                if(array_key_exists($university['provs_id'], $provinces)){
+                    $provinces[$university['provs_id']]['universities'][] = $university;
+                }
+            }
+        }
+        $data['provincs'] = $provinces;
         $this->tplData = $data;
         $this->display("index/index.html");
     }
--- app/models/index_m.php
+++ app/models/index_m.php
@@ -7,30 +7,30 @@ class Index_m extends SB_Model{
     function __construct (){
         parent::__construct();
     }
-    //tag贴子列表 
-    public function get_tag_forums_list($page,$limit,$tag_title){
-            $tag = $this->db->select('tag_id')->where('tag_title',$tag_title)->get('tags')->row_array();
-            if($tag){
-                    $this->db->select('a.fid, a.title, a.comments, a.updatetime, b.uid, b.username, b.avatar')
-                    ->from('forums a')
-                    ->join('users b','a.uid=b.uid')
-                    ->join('tags_relation c','a.fid=c.fid')
-                    ->join('tags d','c.tag_id=d.tag_id')
-                    ->where('d.tag_id',$tag['tag_id'])
-                    ->limit($limit,$page);
-                    $query=$this->db->get();
-                    return $query->result_array();
-            } else {
-                   return false;
-            }
+    
+    // 获取可以展示的所有高校
+    public function get_all_university(){
+        $this->db->select('univs_id, univs_name, provs_id')
+        ->from('university')
+        ->where('status',1);
+        $query=$this->db->get();
+        return $query->result_array();
     }
+    
     //获取所有省份信息
     public function get_all_province(){
+        $result = array();
         $this->db->select('provs_id, provs_name')
         ->from('province');
         $query = $this->db->get();
         if($query->num_rows>0){
-            return $query->result_array();
+            $res = $query->result_array();
         }
+        if(is_array($res) && !empty($res)){
+            foreach($res as $one){
+                $result[$one['provs_id']][] = $one;
+            }
+        }
+        return $result;
     }
 }
