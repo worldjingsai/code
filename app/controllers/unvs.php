d83075b8f20d929cc1e3270e37ad635e097ac134
2822676663@qq.com <2822676663@qq.com>
2014-01-04 16:16:01 +0800

    add

---
+++ app/controllers/unvs.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 高校首页
+ */
+class Unvs extends SB_Controller{
+    function __construct (){
+        parent::__construct();
+        $this->load->library('myclass');
+    }
+     
+    public function index (){
+        $data['date'] = date("m月d日");
+        $this->load->model('index_m');
+        $provinces = $this->index_m->get_all_province();
+        $universities = $this->index_m->get_all_university();
+        if(is_array($universities) && !empty($universities)){
+            foreach($universities as $university){
+                if(array_key_exists($university['provs_id'], $provinces)){
+                    $provinces[$university['provs_id']]['universities'][] = $university;
+                }
+            }
+        }
+        $data['provincs'] = $provinces;
+        $this->tplData = $data;
+        $this->display("index/index.html");
+    }
+}
