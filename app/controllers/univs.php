d839a83f1e686355d33c3580313065eae7946cb2
flag111 <496047736@qq.com>
2014-01-04 16:58:02 +0800

    OK

--- app/controllers/univs.php
+++ app/controllers/univs.php
@@ -8,20 +8,9 @@ class Univs extends SB_Controller{
         $this->load->library('myclass');
     }
      
-    public function index (){
-        $data['date'] = date("m月d日");
-        $this->load->model('index_m');
-        $provinces = $this->index_m->get_all_province();
-        $universities = $this->index_m->get_all_university();
-        if(is_array($universities) && !empty($universities)){
-            foreach($universities as $university){
-                if(array_key_exists($university['provs_id'], $provinces)){
-                    $provinces[$university['provs_id']]['universities'][] = $university;
-                }
-            }
-        }
-        $data['provincs'] = $provinces;
-        $this->tplData = $data;
-        $this->display("index/index.html");
+    public function index ($abc){
+		echo $abc;
+        
+        $this->display("univs/index.html");
     }
 }
