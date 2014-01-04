56e720bc2e57f552a3871fa4a089398bc7be92f6
flag111 <496047736@qq.com>
2014-01-04 23:05:53 +0800

    增加创建竞赛步骤

--- app/controllers/univs.php
+++ app/controllers/univs.php
@@ -17,11 +17,24 @@ class Univs extends SB_Controller{
         $this->display("univs/index.html");
     }
     public function create($univs_id){
+        $step = 1;
+        if(isset($_GET['step']))
+        {
+            $step = intval($_GET['step']);
+        }
+        
         $univs_id = intval($univs_id);
         $this->load->model('univs_m');
         $univs_info = $this->univs_m->get_univs_info_by_univs_id($univs_id);
         $data['university'] = $univs_info;
+        $data['step'] = $step;
         $this->tplData = $data;
-        $this->display("contest/create.html");
+        if ($step == 1)
+        {
+            $this->display("contest/create_1.html");
+        } else {
+            $this->display("contest/create_2.html");
+        }
+        
     }
 }
