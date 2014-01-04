81db455d4d3b48cbd72b2b45302fca8c0339f4bd
flag111 <496047736@qq.com>
2014-01-04 16:56:15 +0800

    OK

--- app/config/routes.php
+++ app/config/routes.php
@@ -9,4 +9,4 @@ $route['qq_callback'] = 'oauth/qqcallback';
 $route['forum/flist/(:num)'] = 'forum/flist/$1';
 $route['forum/view/(:num)'] = 'forum/view/$1';
 $route['tag/index/(:any)'] = 'tag/index/$1';
-$route['univs/(:any)'] = "univs/$1";
+$route['univs/(:num)'] = "univs/index/$1";
