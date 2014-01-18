51e9c8879920a8b36498e1cbf25c4335d172610b
2822676663@qq.com <2822676663@qq.com>
2014-01-18 13:51:43 +0800

    创建UC

---
+++ system/ucclient/uc_client/data/cache/apps.php
@@ -0,0 +1,91 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz! Board',
+    'url' => 'http://www.mathor.com',
+    'ip' => '127.0.0.1',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+  3 => 
+  array (
+    'appid' => '3',
+    'type' => 'DISCUZX',
+    'name' => '玩转PHP',
+    'url' => 'http://www.phpplay.com',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+  6 => 
+  array (
+    'appid' => '6',
+    'type' => 'OTHER',
+    'name' => '校苑团购',
+    'url' => 'http://www.mathor.com/tuangou',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+  10 => 
+  array (
+    'appid' => '10',
+    'type' => 'DISCUZX',
+    'name' => '考研吗信息网',
+    'url' => 'http://www.kaoyanma.com',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+  11 => 
+  array (
+    'appid' => '11',
+    'type' => 'OTHER',
+    'name' => '微博',
+    'url' => 'http://www.mathor.cn',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+);
---
+++ system/ucclient/uc_client/data/cache/badwords.php
@@ -0,0 +1,13 @@
+<?php
+$_CACHE['badwords'] = array (
+  'findpattern' => 
+  array (
+    0 => '/¹²²úµ³/is',
+    1 => '/±ù¶¾/is',
+  ),
+  'replace' => 
+  array (
+    0 => '***',
+    1 => '***',
+  ),
+);
