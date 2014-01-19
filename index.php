7459d4b0e482b08b43defbe2096a6e43a43e8522
2822676663@qq.com <2822676663@qq.com>
2014-01-19 14:40:28 +0800

    映入配置文件

--- index.php
+++ index.php
@@ -141,7 +141,7 @@ if (realpath($system_path) !== FALSE){
 
 // ensure there's a trailing slash
 $system_path = rtrim($system_path, '/').'/';
-
+require_once(APPPATH.'config/constants.php');
 // Is the system path correct?
 if ( ! is_dir($system_path)){
     exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
